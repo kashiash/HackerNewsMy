d0ab12b0e2a0d64b778b1a03fab0828a34ac48f7
Jacek Kosiński <kashiash@gmail.com>
2022-09-06 15:56:39 +0200

    Initial Commit

---
+++ Shared/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  Shared
+//
+//  Created by Jacek Kosinski U on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
